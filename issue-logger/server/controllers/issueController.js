254d941e330b1729f4df8d916cf818eeeec19c86
KeertanaM16 <keerthanna.muthukrishnan@farmsio.com>
2026-02-13 11:38:29 +0530

    Update issue controller

--- issue-logger/server/controllers/issueController.js
+++ issue-logger/server/controllers/issueController.js
@@ -1,7 +1,7 @@
 const db = require('../db');
 
 const createIssue = async (req, res) => {
-  const { issue_text } = req.body;
+  const { issue_text } = req.query;
 
   if (!issue_text || issue_text.trim() === '') {
     return res.status(400).json({ error: 'Issue cannot be empty.' });
